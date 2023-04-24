f2d2cb619931a62d2e3095a1ad4646f9754d3eaf
LetsGitFunky <NickHeinBass@gmail.com>
2023-04-24 14:34:26 -0700

    Int Set complete

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
@@ -14,6 +14,7 @@ require "byebug"
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
@@ -35,12 +36,15 @@ class IntSet
   end
 
   def insert(num)
+    @store[num % 20].push(num)
   end
 
   def remove(num)
+    @store[num % 20].delete(num)
   end
 
   def include?(num)
+    @store[num % 20].include?(num)
   end
 
   private
