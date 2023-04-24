e101940ee0e516048b9cdbc33d56dd919776821c
LetsGitFunky <NickHeinBass@gmail.com>
2023-04-24 16:18:16 -0700

    completed phase 2

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p02_hashing.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p02_hashing.rb
@@ -4,11 +4,23 @@ end
 
 class Array
   def hash
+    new_hash = 0
+    alpha = (0..self.length).to_a
+    self.each_with_index do |ele, idx|
+      new_hash += (alpha.index(ele).hash * idx)
+    end
+    return new_hash
   end
 end
 
 class String
   def hash
+    new_hash = 0
+    alpha = ("a"..'z').to_a
+    self.each_char.with_index do |ele, idx|
+      new_hash += (alpha.index(ele).hash * idx)
+    end
+    return new_hash
   end
 end
 
@@ -16,6 +28,11 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    new_hash = 0
+    alpha = ("a"..'z').to_a
+    self.each do |key, val|
+      new_hash += key.hash * val.to_i.hash
+    end
+    return new_hash
   end
 end
